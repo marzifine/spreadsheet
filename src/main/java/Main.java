1c66fc2de17cb98a9cdcf515511f6c17de21d791
ge54luq <ge54luq@mytum.de>
2021-10-20 16:46:58 +0200

    Replace lambda expression

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,6 @@ public class Main {
     public static void main(String[] args) {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                Application.createAndShowGUI();
-            }
-        });
+        javax.swing.SwingUtilities.invokeLater(Application::createAndShowGUI);
     }
 }
